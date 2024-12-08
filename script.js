d30a717ff74bf3abd58ec1098093bc1f815e355f
Shivani90 <jyotishukla05274@gmail.com>
2024-12-08 21:29:44 +0530

    bn

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ window.onscroll = function () {
   }
 };
 
-// nav hide
+nav hide
 let navBar = document.querySelectorAll(".nav-link");
 let navCollapse = document.querySelector(".navbar-collapse.collapse");
 navBar.forEach(function (a) {
